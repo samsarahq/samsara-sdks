b9fa459477e0241cbdf3b0a472812e167a33ab24
Tyler Freckmann <tyler.freckmann@samsara.com>
2020-01-22 11:46:14 -0800

    Update gen-python-sdk.sh to fix swagger before generating

--- gen-python-sdk.sh
+++ gen-python-sdk.sh
@@ -1,4 +1,5 @@
 rm -f swagger.json
 wget https://raw.githubusercontent.com/samsarahq/api-docs/master/swagger.json
+python3 fix-swagger.py
 rm -rf python-sdk
 openapi-generator generate -i swagger.json -o python-sdk -g python --package-name samsara
