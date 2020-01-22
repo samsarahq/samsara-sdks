4c02ed63df0b47751ac37616e9be953b18450966
Tyler Freckmann <tyler.freckmann@samsara.com>
2020-01-22 11:33:19 -0800

    update gen-python-sdk.sh to generate python sdk from latest swagger

--- gen-python-sdk.sh
+++ gen-python-sdk.sh
@@ -1,1 +1,4 @@
+rm -f swagger.json
 wget https://raw.githubusercontent.com/samsarahq/api-docs/master/swagger.json
+rm -rf python-sdk
+openapi-generator generate -i swagger.json -o python-sdk -g python --package-name samsara
