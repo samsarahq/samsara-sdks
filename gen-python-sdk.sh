723649c1158eb53c71355ffca8e525323566714e
Tyler Freckmann <tyler.freckmann@samsara.com>
2020-01-22 17:54:37 -0800

    update gen-python-sdk.sh to use fix-it scripts

--- gen-python-sdk.sh
+++ gen-python-sdk.sh
@@ -1,5 +1,11 @@
+# Grabs most recent swagger
 rm -f swagger.json
 wget https://raw.githubusercontent.com/samsarahq/api-docs/master/swagger.json
-python3 fix-swagger.py
+
+# Fix swagger
+python3 fix-description.py
+python3 fix-inline-schemas.py
+
+# Generate python sdk off of fixed swagger
 rm -rf python-sdk
 openapi-generator generate -i swagger.json -o python-sdk -g python --package-name samsara
