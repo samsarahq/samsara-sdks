e86ef6b1491fcc795437ec78e8e89a0a35bbfbbd
Tyler Freckmann <tyler.freckmann@samsara.com>
2020-01-23 17:12:16 -0800

    Update examples

--- examples/create_route.py
+++ examples/create_route.py
@@ -1,58 +1,61 @@
 from __future__ import print_function
 import datetime
-import openapi_client
-from openapi_client.rest import ApiException
+import samsara
+from samsara.rest import ApiException
 
-
-# Create an instance of the openapi_client
-# configuration = openapi_client.Configuration()
+# Create an ApiClient
 with open('token', 'r') as f:
-    # configuration.api_key_prefix["Authorization"] = "Bearer"
-    # configuration.api_key["Authorization"] = token.read()
     token = f.read()
-client = openapi_client.ApiClient(header_name='Authorization', header_value=f'Bearer {token}')
+client = samsara.ApiClient(header_name='Authorization', header_value=f'Bearer {token}')
 
 # Create an instance of some APIs
-routes_api = openapi_client.RoutesApi(api_client=client)
-addresses_api = openapi_client.AddressesApi(api_client=client)
+routes_api = samsara.RoutesApi(api_client=client)
+addresses_api = samsara.AddressesApi(api_client=client)
 
-# Get addresses
-addresses = []
-try:
-    cursor = None
-    has_next_page = True
-    while has_next_page:
-        resp = addresses_api.get_addresses(after=cursor)
-        addresses.extend(resp.data)
-        cursor = resp.pagination.end_cursor
-        has_next_page = resp.pagination.has_next_page
-except ApiException as e:
-    print(e)
+# Create some addresses for use in a route
+address_ids = {}
+sf6 = samsara.AddressCreate(
+    name="SF6",
+    formatted_address="350 Rhode Island St, San Francisco, CA",
+    geofence=samsara.AddressGeofenceRequest(
+        circle=samsara.AddressGeofenceRequestCircle(
+            radius_meters=25
+        )
+    )
+)
+resp = addresses_api.create_address(address=sf6)
+address_ids['SF6'] = resp.data.id
 
-# Search for addresses function
-def find_address_id(name, addresses):
-    return list(filter(lambda address: address.name == name, addresses))[0].id
+sj1 = samsara.AddressCreate(
+    name="SJ1",
+    formatted_address="99 S Almaden Blvd, San Jose, CA",
+    geofence=samsara.AddressGeofenceRequest(
+        circle=samsara.AddressGeofenceRequestCircle(
+            radius_meters=25
+        )
+    )
+)
+resp = addresses_api.create_address(address=sj1)
+address_ids['SJ1'] = resp.data.id
 
-# Convert to from datetime to milliseconds
+# Helper func: Convert from datetime object to unix ms
 def ms(datetime):
     return int(datetime.timestamp()*1000)
 
-# Create a route from SF6 to SJ1 and then delete it
-try:
-    route = openapi_client.V1DispatchRouteCreate(
-        dispatch_jobs=[
-            openapi_client.V1DispatchJobCreate(
-                destination_address_id=find_address_id('SF6', addresses),
-                scheduled_arrival_time_ms=ms(datetime.datetime.now() + datetime.timedelta(days=1, hours=1))
-            )
-        ],
-        name="SF6 to SJ1",
-        scheduled_start_ms=ms(datetime.datetime.now() + datetime.timedelta(days=1)),
-        start_location_address_id=find_address_id('SJ1', addresses)
-    )
-    resp = routes_api.v1create_dispatch_route(route)
-    print(resp)
-    resp = routes_api.v1delete_dispatch_route_by_id(resp.id)
-    print(resp)
-except ApiException as e:
-    print(e)
+# Create a route from SF6 to SJ1 and then delete everything for cleanup
+route = samsara.V1DispatchRouteCreate(
+    name="SF6 to SJ1",
+    start_location_address_id=address_ids['SF6'],
+    scheduled_start_ms=ms(datetime.datetime.now() + datetime.timedelta(days=1)),
+    dispatch_jobs=[
+        samsara.V1DispatchJobCreate(
+            destination_address_id=address_ids['SJ1'],
+            scheduled_arrival_time_ms=ms(datetime.datetime.now() + datetime.timedelta(days=1, hours=1))
+        )
+    ],
+)
+resp = routes_api.v1create_dispatch_route(route)
+print(resp)
+routes_api.v1delete_dispatch_route_by_id(resp.id)
+addresses_api.delete_address_by_id(address_ids['SF6'])
+addresses_api.delete_address_by_id(address_ids['SJ1'])
--- examples/get_vehicle_locations.py
+++ examples/get_vehicle_locations.py
@@ -1,23 +1,20 @@
 from __future__ import print_function
 import time
-import openapi_client
-from openapi_client.rest import ApiException
+import samsara
+from samsara.rest import ApiException
 
-# Create an instance of the openapi_client
-# configuration = openapi_client.Configuration()
+# Create an ApiClient
 with open('token', 'r') as f:
-    # configuration.api_key_prefix["Authorization"] = "Bearer"
-    # configuration.api_key["Authorization"] = token.read()
     token = f.read()
-client = openapi_client.ApiClient(header_name='Authorization', header_value=f'Bearer {token}')
+client = samsara.ApiClient(header_name='Authorization', header_value=f'Bearer {token}')
 
 # Create an instance of the VehiclesAPI
-api = openapi_client.VehiclesApi(api_client=client)
+api = samsara.VehiclesApi(api_client=client)
 
 # Get vehicle locations
-try:
-    cursor = None
-    for i in range(5):
+cursor = None
+for i in range(5):
+    try:
         resp = api.get_vehicle_locations_feed(after=cursor)
         for vehicle in resp.data:
             print(f'New data available for: {vehicle.name}!')
@@ -32,5 +29,5 @@ try:
         if not resp.pagination.has_next_page:
             print('Waiting for new data...')
             time.sleep(5)
-except ApiException as e:
-    print(e)
+    except ApiException as e:
+        print(e)
